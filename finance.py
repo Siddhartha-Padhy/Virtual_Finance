0fa0506784361d389cb54101ac54df78d75b883e
Siddhartha-Padhy <siddhartha6222@gmail.com>
2022-03-02 23:32:42 +0530

    first commit

---
+++ finance.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, redirect, url_for, request
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/home')
+def home_page():
+    return render_template('home.html')
+
+@app.route('/bank')
+def bank_page():
+    return render_template('bank.html')
+
+@app.route('/stock')
+def stock_page():
+    return render_template('stock.html')
+
+@app.route('/contacts')
+def contacts_page():
+    return render_template('contacts.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
