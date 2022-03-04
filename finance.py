4850072542ec3465df1e2a4a642c8e379dece873
Siddhartha-Padhy <siddhartha6222@gmail.com>
2022-03-04 23:08:42 +0530

    add index.html

--- finance.py
+++ finance.py
@@ -2,6 +2,9 @@ from flask import Flask, render_template, redirect, url_for, request
 app = Flask(__name__)
 
 @app.route('/')
+def index_page():
+    return render_template('index.html')
+
 @app.route('/home')
 def home_page():
     return render_template('home.html')
